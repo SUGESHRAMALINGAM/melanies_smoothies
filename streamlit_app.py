37e435dd5cd815e7846acb730ce119c5fff4de84
SUGESH R <101317215+SUGESHRAMALINGAM@users.noreply.github.com>
2024-09-21 15:45:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("Custamize Your Smoothie :balloon:")
@@ -36,4 +37,6 @@ if ingredients_list:
 if len(ingredients_list) > 5:
     st.warning("You can only select up to 5 options. Remove an option first.")
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+st.text(fruityvice_response.json())

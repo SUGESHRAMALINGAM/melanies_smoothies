ad56f0933cb94d7e9de7b1fcb32e5db0ce5ae252
SUGESH R <101317215+SUGESHRAMALINGAM@users.noreply.github.com>
2024-09-21 15:54:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,8 @@ if ingredients_list:
     ingredients_string =''
     for fruit_chosen in ingredients_list: 
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") 
+        fv_df = st.dataframe (data=fruityvice_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) 
                     values ('""" +ingredients_string+ """','""" +name_on_order+ """')"""
@@ -33,10 +35,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-# Show a message if more than 5 ingredients are selected
+
 if len(ingredients_list) > 5:
     st.warning("You can only select up to 5 options. Remove an option first.")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") #st.text(fruityvice_response.json())
 
-fv_df = st.dataframe (data=fruityvice_response.json(), use_container_width=True)
